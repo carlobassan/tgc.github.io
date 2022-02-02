6f43f14162a9cdc0c9384530c7f93873213b10a2
carlobassan <76621878+carlobassan@users.noreply.github.com>
2022-02-02 15:15:20 +0100

    creato un file js unico

---
+++ script.js
@@ -0,0 +1,8 @@
+  $(document).ready(function() {
+    /* Open Panel */
+    $(".hamburger").on('click', function() {
+      $(".menu").toggleClass("menu--open", 500, 'easeOutCubic');
+      $(".hamburger").toggleClass("hamburger--open");
+      $("body").toggleClass("body--fixed")
+    });
+  })
